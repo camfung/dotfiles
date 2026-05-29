afd443d1f81813f573d3df1928a5ca2095427a03
camfung <camf@caracal-tech.com>
2026-05-29 14:10:58 -0700

    Add -m/--move flag to obslink to mv instead of ln

--- functions/obslink.zsh
+++ functions/obslink.zsh
@@ -1,7 +1,16 @@
 obslink() {
+  local move=0
+  while [[ "$1" == -* ]]; do
+    case "$1" in
+      -m|--move) move=1; shift ;;
+      *) echo "Unknown option: $1"; return 1 ;;
+    esac
+  done
+
   if [ $# -eq 0 ]; then
-    echo "Usage: obslink <file_or_directory> [link_name]"
+    echo "Usage: obslink [-m|--move] <file_or_directory> [link_name]"
     echo "Creates a symbolic link from <file_or_directory> to ~/Documents/obsidian-vault/"
+    echo "With -m|--move, moves the file instead of linking it"
     echo "If link_name is not provided, uses the file/directory name"
     return 1
   fi
@@ -33,12 +42,24 @@ obslink() {
     rm -rf "$link_path"
   fi
 
-  ln "$abs_source_path" "$link_path"
+  if [ "$move" -eq 1 ]; then
+    mv "$abs_source_path" "$link_path"
+  else
+    ln "$abs_source_path" "$link_path"
+  fi
 
   if [ $? -eq 0 ]; then
-    echo "Successfully linked '$abs_source_path' to '$link_path'"
+    if [ "$move" -eq 1 ]; then
+      echo "Successfully moved '$abs_source_path' to '$link_path'"
+    else
+      echo "Successfully linked '$abs_source_path' to '$link_path'"
+    fi
   else
-    echo "Failed to create link"
+    if [ "$move" -eq 1 ]; then
+      echo "Failed to move"
+    else
+      echo "Failed to create link"
+    fi
     return 1
   fi
 }
