d7accd04f9eaf8cd9df1382ad82134097da4eeed
camfung <camf@caracal-tech.com>
2026-06-03 11:32:20 -0700

    obslink: hard link by default, symlink for dirs and -s flag
    Fix stale docs (said symbolic, was hard linking). Dirs now force
    symlink since hard links can't span directories. Add -s|--symbolic
    flag to symlink a file instead of hard linking.

--- functions/obslink.zsh
+++ functions/obslink.zsh
@@ -1,16 +1,32 @@
 obslink() {
   local move=0
+  local symbolic=0
   while [[ "$1" == -* ]]; do
     case "$1" in
       -m|--move) move=1; shift ;;
+      -s|--symbolic) symbolic=1; shift ;;
+      -h|--help)
+        echo "Usage: obslink [-m|--move] [-s|--symbolic] <file_or_directory> [link_name]"
+        echo "Creates a hard link from <file_or_directory> to ~/Documents/obsidian-vault/"
+        echo
+        echo "Options:"
+        echo "  -m, --move      Move the file instead of linking it"
+        echo "  -s, --symbolic  Create a symbolic link instead of a hard link"
+        echo "  -h, --help      Show this help message"
+        echo
+        echo "Directories are always linked symbolically (hard links can't span directories)."
+        echo "If link_name is not provided, uses the file/directory name"
+        return 0 ;;
       *) echo "Unknown option: $1"; return 1 ;;
     esac
   done
 
   if [ $# -eq 0 ]; then
-    echo "Usage: obslink [-m|--move] <file_or_directory> [link_name]"
-    echo "Creates a symbolic link from <file_or_directory> to ~/Documents/obsidian-vault/"
+    echo "Usage: obslink [-m|--move] [-s|--symbolic] <file_or_directory> [link_name]"
+    echo "Creates a hard link from <file_or_directory> to ~/Documents/obsidian-vault/"
     echo "With -m|--move, moves the file instead of linking it"
+    echo "With -s|--symbolic, creates a symbolic link instead of a hard link"
+    echo "Directories are always linked symbolically"
     echo "If link_name is not provided, uses the file/directory name"
     return 1
   fi
@@ -42,8 +58,14 @@ obslink() {
     rm -rf "$link_path"
   fi
 
+  if [ -d "$abs_source_path" ]; then
+    symbolic=1
+  fi
+
   if [ "$move" -eq 1 ]; then
     mv "$abs_source_path" "$link_path"
+  elif [ "$symbolic" -eq 1 ]; then
+    ln -s "$abs_source_path" "$link_path"
   else
     ln "$abs_source_path" "$link_path"
   fi
@@ -51,6 +73,8 @@ obslink() {
   if [ $? -eq 0 ]; then
     if [ "$move" -eq 1 ]; then
       echo "Successfully moved '$abs_source_path' to '$link_path'"
+    elif [ "$symbolic" -eq 1 ]; then
+      echo "Successfully symlinked '$abs_source_path' to '$link_path'"
     else
       echo "Successfully linked '$abs_source_path' to '$link_path'"
     fi
